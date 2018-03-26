41cdcadc020cb74d4975a612c1a6337cf704e73d
Rio7z <kane@riotz.co.uk>
2018-03-26 12:21:50 +0100

    v3

---
+++ scripts/main.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){$("#fullpage").fullpage(),tl=new TimelineMax,tlUser=new TimelineMax,tl.from($(".line1"),.5,{y:50,autoAlpha:0,ease:Power1.easeOut}).from($(".line2"),.5,{y:50,autoAlpha:0,ease:Power1.easeOut}).from($("#O"),.5,{y:-50,autoAlpha:0,ease:Power3.easeOut}).from($("#R"),.5,{autoAlpha:0,ease:Power3.easeOut}).from($("#I"),.5,{autoAlpha:0,ease:Power3.easeOut}).from($("#T"),.5,{autoAlpha:0,ease:Power3.easeOut}).from($("#Z"),.5,{autoAlpha:0,ease:Power3.easeOut}).from($("#D"),.5,{autoAlpha:0,ease:Power3.easeOut})});
+//# sourceMappingURL=main.js.map
---
+++ scripts/main.src.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+$('#fullpage').fullpage();
+
+tl = new TimelineMax();
+tlUser = new TimelineMax();
+
+tl
+  .from($('.line1'), 0.5,  {y: 50, autoAlpha: 0, ease: Power1.easeOut})
+  .from($('.line2'), 0.5,  {y: 50, autoAlpha: 0, ease: Power1.easeOut})
+  .from($('#O'), 0.5,  {y: -50, autoAlpha: 0, ease: Power3.easeOut})
+  .from($('#R'), 0.5,  {autoAlpha: 0, ease: Power3.easeOut})
+  .from($('#I'), 0.5,  {autoAlpha: 0, ease: Power3.easeOut})
+  .from($('#T'), 0.5,  {autoAlpha: 0, ease: Power3.easeOut})
+  .from($('#Z'), 0.5,  {autoAlpha: 0, ease: Power3.easeOut})
+  .from($('#D'), 0.5,  {autoAlpha: 0, ease: Power3.easeOut})
+
+});
